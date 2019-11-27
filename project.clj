7b1289fc487314548e1e8f348fe51532211c1993
Phillippe Siclait <lsiclait@gmail.com>
2019-11-27 09:31:08 -0500

    Add incompatible dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.google.cloud/google-cloud-pubsub "1.100.0"]
+                 [datasplash "0.6.6"]]
   :main ^:skip-aot dependency-conflict.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
